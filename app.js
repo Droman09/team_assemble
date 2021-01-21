b8d96141b66a929aeb5ad6da54cb5a61b815656a
David Roman <david_roman09@yahoo.com>
2021-01-21 16:08:37 -0600

    prompt questions for user

--- app.js
+++ app.js
@@ -5,10 +5,147 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+var employeeArr = []
+
+function employeeInfo() {
+     inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Identify the employee position",
+                choices: ["Manager", "Engineer", "Intern"],
+                name: "position"
+            }
+        ]).then(data => {
+            if (data.position === "Manager") {
+                 return managerInfo();
+            } else if (data.position === "Engineer") {
+                return engineerInfo();
+            } else if (data.position === "Intern") {
+                return internInfo()
+            } else {
+                console.log("please select one")
+            } ///refactor this 
+        })
+        
+}
+
+
+function managerInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Name of Manager",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "ID of Manager",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Email of Manager",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Office number?",
+                name: "officeNumber"
+            },
+            
+        ])
+        .then(data =>{ var newManager = new Manager(data.name, data.id, data.email, data.officeNumber);
+            employeeArr.push(newManager);
+            console.log(employeeArr)
+        })
+        
+}
+
+function engineerInfo() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Name of Engineer",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "ID of Engineer",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Email of Engineer",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Github username?",
+                name: "github"
+            }
+        ]).then(data =>{ var newEngineer = new Engineer(data.name, data.id, data.email, data.github);
+            employeeArr.push(newEngineer);
+            console.log(newEngineer)
+        })
+}
+
+function internInfo() {
+    inquirer
+        .prompt([
+             {
+                type: "input",
+                message: "Name of Intern",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "ID of Intern",
+                name: "id"
+            },
+            {
+                type: "input",
+                message: "Email of Intern",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Intern School",
+                name: "school"
+            }
+        ]).then(data =>{ var newIntern = new Intern(data.name, data.id, data.email, data.school);
+            employeeArr.push(newIntern);
+            console.log(newIntern)
+        })
+}
+
+function addNew(){
+    inquirer
+    .prompt([
+        {
+            type: "confirm",
+            name: "choice",
+            message: "Add another employee?"
+        } 
+    ]).then(data =>{ 
+            if (data.choice){
+                this.employeeInfo()
+            } else {
+                console.log("avenger assemble!!")
+            }
+    })
+}
+
+employeeInfo()
+
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -28,8 +165,4 @@ const render = require("./lib/htmlRenderer");
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+
